dc06f11133318e3a5e26a3952149bb93095b1fb9
Byte Cats <devin@bytecats.codes>
2022-11-20 23:07:20 -0500

    file functions

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/byte-cats/filechick
 
 go 1.19
+
+require github.com/spf13/afero v1.9.3
+
+require golang.org/x/text v0.3.4 // indirect
---
+++ requester.go
@@ -0,0 +1,42 @@
+package filechick
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+// RandomizeUserAgent randomize the user agent
+func RandomizeUserAgent() string {
+	return "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"
+}
+
+// ApplyUserAgent function to apply the user agent to a http request
+func ApplyUserAgent(req *http.Request) {
+	req.Header.Add("User-Agent", RandomizeUserAgent())
+}
+
+func CustomRequest(url string) (string, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "Something went wrong with request", err
+	}
+	ApplyUserAgent(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "Something went wrong with the request", err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing body:", err)
+		}
+	}(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "Error reading body:", err
+	}
+	return string(body), nil
+}
