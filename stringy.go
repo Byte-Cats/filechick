66d4fed956ef452ae042390a4db6bf8c43849548
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-05 11:20:39 -0500

    Create stringy.go

---
+++ stringy.go
@@ -0,0 +1,12 @@
+package filechick 
+
+// StringToInt to change string to int
+func StringToInt(str string) int {
+	var i int
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println("Error converting string to integer:", err)
+		return 0
+	}
+	return i
+}
