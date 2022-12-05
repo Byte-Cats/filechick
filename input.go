a4f322527953a83f6e2d09c669a95cbe00d51dff
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-05 11:20:05 -0500

    Update input.go
    will move this into its own fs

--- input.go
+++ input.go
@@ -23,13 +23,3 @@ func GetInput() string {
 	return input
 }
 
-// StringToInt to change string to int
-func StringToInt(str string) int {
-	var i int
-	i, err := strconv.Atoi(str)
-	if err != nil {
-		fmt.Println("Error converting string to integer:", err)
-		return 0
-	}
-	return i
-}
