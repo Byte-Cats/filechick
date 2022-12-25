6a8ab71b549afbad7479f034171b286814993d5e
4ce <88108711+4cecoder@users.noreply.github.com>
2022-12-25 01:56:53 -0500

    Update alchemy.go

--- alchemy.go
+++ alchemy.go
@@ -14,23 +14,43 @@ func GenerateQR(size int, filename string, content string) {
 	qr.WriteFile(size, filename)
 }
 
+// GenerateLotteryNumbers generates a slice of unique integers from 1 to num.
+// num is the number of integers to generate.
+// Returns the slice of generated integers.
 func GenerateLotteryNumbers(num int) []int {
+	// Initialize an empty slice of integers with a capacity of num.
 	lotteryNumbers := make([]int, 0, num)
+
+	// Generate num unique integers from 1 to num.
 	for len(lotteryNumbers) < num {
+		// Generate a random integer from 1 to num.
 		n := rand.Intn(num) + 1
+
+		// Check if the generated integer is already in the slice.
 		if !contains(lotteryNumbers, n) {
+			// If the integer is not in the slice, append it to the slice.
 			lotteryNumbers = append(lotteryNumbers, n)
 		}
 	}
+
+	// Return the slice of generated integers.
 	return lotteryNumbers
 }
 
+// contains checks if an integer is in a slice of integers.
+// s is the slice of integers to check.
+// e is the integer to check for.
+// Returns true if e is in s, false otherwise.
 func contains(s []int, e int) bool {
+	// Iterate through the slice of integers.
 	for _, a := range s {
+		// Check if the current integer is equal to e.
 		if a == e {
+			// If the integers are equal, return true.
 			return true
 		}
 	}
+	// If no integers in the slice are equal to e, return false.
 	return false
 }
 
